cd0231015a93c6a91973844c116582ac24a1f339
Youded-byte <82912625+Youded-byte@users.noreply.github.com>
2022-02-19 01:04:02 +0100

    Update utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -29,12 +29,12 @@ QString Utils::getAgentFlags(const QString &name) {
     if (getAgentExists(name))
         return QStringLiteral("-javaagent:") + getAgentsDirectory() + QDir::separator() + name;
     else
-        return QStringLiteral("");
+        return null;
 }
 
 QString Utils::getAgentFlags(const QString &name, const QString &option) {
     if (getAgentExists(name))
         return getAgentFlags(name) + "=" + option;
     else
-        return QStringLiteral("");
+        return null;
 }
